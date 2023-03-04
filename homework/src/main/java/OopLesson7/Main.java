3369c0c4241ab14458587c09823fde801ae662e2
Anna Zatsepina <anka.zatcepina@gmail.com>
2023-03-04 21:20:43 +0300

    Parser added

--- homework/src/main/java/OopLesson7/Main.java
+++ homework/src/main/java/OopLesson7/Main.java
@@ -1,7 +1,10 @@
 package OopLesson7;
 
+import java.text.SimpleDateFormat;
+
 import OopLesson7.controller.*;
 import OopLesson7.model.IRepository;
+import OopLesson7.model.ParserTxt;
 import OopLesson7.model.RepositoryFolderTxt;
 import OopLesson7.view.*;
 
@@ -10,7 +13,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        IRepository repository = new RepositoryFolderTxt("source//notesTxt");
+        IRepository repository = new RepositoryFolderTxt("source//notesTxt", new ParserTxt(new SimpleDateFormat("dd.MM.yyyy")));
         NoteController controller = new NoteController(repository);
         IView view = new ViewConsole(controller);
         view.run();
